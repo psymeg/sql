b13a07c9970636a6cedc7dac06204e81ad9b6012
psymeg <terryteo@gmail.com>
2018-07-01 08:34:40 +1200

    location files and createdb

--- sales_items.sql
+++ sales_items.sql
@@ -1,9 +1,10 @@
 CREATE TABLE sales_items (
   item_id INTEGER NOT NULL, 
-  so_id INTEGER REFERENCES so_headers(id),
+  so_id INTEGER,
   product_id INTEGER,
   qty INTEGER,
   net_price numeric,
-  PRIMARY KEY (item_id,so_id)
+  PRIMARY KEY (item_id, so_id),
+  FOREIGN KEY (so_id) REFERNCES so_headers (id)
 );
 
